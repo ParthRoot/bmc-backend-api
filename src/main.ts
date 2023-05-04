ebc0861d74919b9ed4db681665e9f1337e9f35fd
shiv-dadhania <shiv.dadhania@scaletech.xyz>
2023-05-04 14:09:34 +0530

    added swagger doc

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { GlobalExceptionFilter } from './utils/dispatchers/exception.dispatchers
 import { TransformInterceptor } from './utils/dispatchers/transform.dispatchers';
 import { ValidationPipe } from './utils/pipe/validation.pipe';
 import { RequestLoggerMiddleware } from './utils';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const compression = require('compression');
 async function bootstrap() {
@@ -30,8 +31,14 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalPipes(new ValidationPipe());
 
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('BMC BACKEND API')
+    .setDescription('User Endpoint')
+    .setVersion('1.0');
+  const document = SwaggerModule.createDocument(app, documentBuilder.build());
+  SwaggerModule.setup('docs/bmc', app, document);
+
   const port = getEnv('BACKEND_PORT');
-  // const port = 7007;
   app.listen(port, () =>
     console.log(`Server started port=${port}, url=http://0.0.0.0:${port}, env=${currentEnv()}`),
   );
