96c4f20769699683d450f8749ca3d6a9f1b8a270
rita-scaletech <rita.savaliya@scaletech.xyz>
2023-05-05 18:01:48 +0530

    login done with swagger add

--- src/users/common/dto/res/users.login.response.dto.ts
+++ src/users/common/dto/res/users.login.response.dto.ts
@@ -1,8 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UsersLoginResDto {
   token?: string | undefined;
+
+  @ApiProperty({
+    name: 'message',
+    description: 'message',
+    type: 'string',
+    required: true,
+})
   message : string;
 
   constructor(  token?: string | undefined, message?: string | undefined) {
+    
     this.token = token;
     this.message = message;
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersLoginReqDto } from './common/dto/req/users.login.request.dto';
-import { AvailableRoleEnum, RoleGuard } from 'src/utils';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { UsersLoginResDto } from './common/dto/res';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
+  @ApiTags('users')
+  @ApiCreatedResponse({
+    description: 'user login successfully',
+    type: UsersLoginResDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'user cannot found',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'user cannot login',
+  })
   @Post('login')
   async userLogin(
     @Body() userloginDto : UsersLoginReqDto
